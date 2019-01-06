7dfc652ddf9f1a5ba519ec15d98fc4fc37d85620
Axelandre42 <axelandre42@yahoo.fr>
2019-01-06 01:09:35 +0100

    Renamed events package to event

--- src/main/java/ovh/axelandre42/railcraftdiesel/common/event/ServerEventHandler.java
+++ src/main/java/ovh/axelandre42/railcraftdiesel/common/event/ServerEventHandler.java
@@ -20,7 +20,7 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package ovh.axelandre42.railcraftdiesel.common.events;
+package ovh.axelandre42.railcraftdiesel.common.event;
 
 import net.minecraft.server.integrated.IntegratedServer;
 import net.minecraft.util.text.TextComponentString;
